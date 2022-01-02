d22d912ee514f03b92d673eefddf1766770fe96a
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-02 22:06:00 +0500

    242

--- 242.py
+++ 242.py
@@ -1,7 +1,24 @@
+
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+        s = "".join(sorted(list(s)))
+        t = "".join(sorted(list(t)))
+
+        return s==t
+
+
+
+
+
+
+# def isAnagram(s,t):
+#
+#     # nat = list(s)
+#     # sort = sorted(list(s))
+#     s = "".join(sorted(list(s)))
+#     t = "".join(sorted(list(t)))
+#     return t == s
+#
+# a = isAnagram("rat", "car")
+#
+# print(a)
