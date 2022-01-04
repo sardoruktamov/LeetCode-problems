56b7514f46abc443768e87426696cf56323257c1
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-04 19:03:38 +0500

    1470-e

--- 1470.py
+++ 1470.py
@@ -1,13 +1,28 @@
-def isAnagram(nums, n):
-    result = []
-    for i in range(n):
-        result.append(nums[i])
-        result.append(nums[i+n])
-    return result
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[i+n])
+        return result
 
 
-nums = [2,5,1,3,4,7]
-n = 3
-a = isAnagram(nums, n)
-
-print(a)
+## kodli test qilish qismi
+# def isAnagram(nums, n):
+#     result = []
+#     for i in range(n):
+#         result.append(nums[i])
+#         result.append(nums[i+n])
+#     return result
+#
+#
+# nums = [2,5,1,3,4,7]
+# n = 3
+# a = isAnagram(nums, n)
+#
+# print(a)
