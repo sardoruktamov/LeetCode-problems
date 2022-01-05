3cc8ea1472a90bfc45cd8604df4a22f5327dad3c
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-05 21:17:14 +0500

    create

---
+++ 1108-e.py
@@ -0,0 +1,15 @@
+
+# ## kodli test qilish qismi
+# def isAnagram(address):
+#
+#     dot = "[.]"
+#     for i in address:
+#         if i == ".":
+#             r = address.replace(".", dot)
+#             return r
+#
+#
+# address = "1.1.1.1"
+# a = isAnagram(address)
+#
+# print(a)
