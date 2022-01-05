842161fa63db712904f79cc3d59e6e3feaa043fb
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-05 21:18:53 +0500

    truncateSentence

--- 1816-e.py
+++ 1816-e.py
@@ -1,3 +1,17 @@
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        s = s.split(" ")
+        list = []
+        result = " "
+        for i in range(k):
+            list.append(s[i])
+            result.join(s[i])
+        return (result.join(list))
 
 ## kodli test qilish qismi
 # def isAnagram(s,k):
