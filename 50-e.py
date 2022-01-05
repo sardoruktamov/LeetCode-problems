63698592a0f133294510d444bb9f710f6fb95bc8
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-05 21:19:57 +0500

    create

---
+++ 50-e.py
@@ -0,0 +1,13 @@
+
+# import math
+# ## kodli test qilish qismi
+# def isAnagram(x, n):
+#     result = math.pow(x,n)
+#     return result
+#
+#
+# x = 2
+# n = -2
+# a = isAnagram(x, n)
+#
+# print(a)
