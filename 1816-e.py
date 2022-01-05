e6dbbbb839d61ea28dfebfcd01591f31c5b6d11f
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-05 21:18:22 +0500

    create

---
+++ 1816-e.py
@@ -0,0 +1,15 @@
+
+## kodli test qilish qismi
+# def isAnagram(s,k):
+#     s = s.split(" ")
+#     list = []
+#     result = " "
+#     for i in range(k):
+#         list.append(s[i])
+#         result.join(s[i])
+#     return (result.join(list))
+#
+# s = "chopper is not a tanuki"
+# k = 5
+# a = isAnagram(s,k)
+# print(a)
