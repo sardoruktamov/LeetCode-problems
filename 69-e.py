9fb2d748ec26c2daaf91a5725c5f6f893dd63f06
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-01-06 20:49:37 +0500

    create

---
+++ 69-e.py
@@ -0,0 +1,12 @@
+
+# import math
+# ## kodli test qilish qismi
+# def isAnagram(x):
+#     result = int(math.floor(math.sqrt(x)))
+#     return result
+#
+#
+# x = 8
+# a = isAnagram(x)
+#
+# print(a)
